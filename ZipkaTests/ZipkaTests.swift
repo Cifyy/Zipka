6e6d6c59ce31a5baac0f041c1ab398e1c21ef801
Cifyy <kuba.majka.pl@gmail.com>
2025-12-04 21:00:40 +0100

    Initial Commit

---
+++ ZipkaTests/ZipkaTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZipkaTests.swift
+//  ZipkaTests
+//
+//  Created by Jakub Majka on 4/12/25.
+//
+
+import Testing
+@testable import Zipka
+
+struct ZipkaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
